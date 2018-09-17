3e2f9ba85bd8aa7305a244378c4e88911133d7c8
Ian McCausland <imccausl@gmail.com>
2018-09-16 22:55:44 -0400

    Initial commit

---
+++ contentScript.js
@@ -0,0 +1,22 @@
+const targetNode = document.getElementById('flux-app');
+const config = {
+  attributes: false,
+  childList: true,
+  subtree: true,
+};
+
+function mutationHandler(mutationsList) {
+  const editableParentElement = document.querySelectorAll('.public-DraftEditor-content');
+  
+  if (editableParentElement.length > 0) {
+    editableParentElement.forEach(elm => {
+      if (!elm.spellchek) {
+        elm.spellcheck = true;
+      }
+    });
+  }
+}
+
+const observer = new MutationObserver(mutationHandler);
+
+observer.observe(targetNode, config);
